ebaab34d8f1812ff027cebb4cee1a4577a40455c
Aleksandr Muravja <alex@kyberorg.io>
2022-08-19 10:17:19 +0300

    RC4

--- cmd/harbor-scan-report/github/step_comment.go
+++ cmd/harbor-scan-report/github/step_comment.go
@@ -12,11 +12,11 @@ func WriteStepComment(scanReport *scan.Report) {
 	//append to GITHUB_STEP_SUMMARY env
 	report = scanReport
 
-	currentStepSummary := os.Getenv(StepSummaryEnvVar)
-	err := os.Setenv(StepSummaryEnvVar, currentStepSummary+"## It works!")
+	//currentStepSummary := os.Getenv(StepSummaryEnvVar)
+	err := os.Setenv(StepSummaryEnvVar, "## It works!")
 	if err != nil {
 		log.Warning.Printf("Failed to write Step Comment. Error: %s", err.Error())
 	}
 
-	log.Debug.Printf("Current value: %s /n", os.Getenv(StepSummaryEnvVar))
+	log.Debug.Printf("Current value: %s \n", os.Getenv(StepSummaryEnvVar))
 }
